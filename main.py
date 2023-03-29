7bacc50422f7c266f95ff8a145ff4987779284a1
Renan Araújo <renan.araujoonline@gmail.com>
2023-03-29 10:40:57 -0300

    vibora now supports args! check file.py help

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 from pdf2image import convert_from_path, convert_from_bytes
 import os
+import time
+import sys
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
@@ -10,7 +12,28 @@ from pdf2image.exceptions import (
     PDFPopplerTimeoutError
 )
 
-images = convert_from_path(r"C:\Users\Renan\Desktop\vibora\RenanAraujoCurrículo.pdf")
+def pdf_to_png(pdf_path):
+  # example: images = convert_from_path(r'C:/path/to/file.pdf')
+  images = convert_from_path(pdf_path)
+  for i in range(len(images)):
+    # this is the name of converted file. change that later, too weird a file always called page[number]
+    images[i].save('page' + str(i) + '.png')
 
-for i in range(len(images)):
-  images[i].save('page' + str(i) + '.png')
+if __name__ == '__main__':
+  # case we type python main.py
+  if len(sys.argv) == 1:
+    print("Unknown command, see `python main.py help` for reference on how to use it!")
+  # case we type python main.py help
+  elif sys.argv[1].lower() == 'help':
+    print('\nWelcome to vibora :) A PDF tool that lets you convert a PDF to PNG, PDF to text, plus some more awesome things!\n\nPDF TO PNG:\nto convert a .PDF to .PNG, use: python main.py pdf2png file.pdf')
+    exit()
+  # case we actually pass a valid argument
+  else:
+    command = sys.argv[1].lower()
+    # e.g. python main.py pdf2png | note that it also expects a file! -> python main.py pdf2png file.pdf
+    if command == 'pdf2png':
+      pdf_path = sys.argv[2]
+      print("converting")
+      pdf_to_png(pdf_path)
+      time.sleep(2)
+      print('file converted!')
