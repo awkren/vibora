e8a47c8ffb4b842acf2096aa588b26088c3e2de3
Renan Araújo <renan.araujoonline@gmail.com>
2023-03-30 07:16:24 -0300

    pdf2png: added check for inexistent file

--- main.py
+++ main.py
@@ -22,18 +22,29 @@ def pdf_to_png(pdf_path):
 if __name__ == '__main__':
   # case we type python main.py
   if len(sys.argv) == 1:
-    print("Unknown command, see `python main.py help` for reference on how to use it!")
+    print("Missign arguments, see `python main.py help` for reference on how to use it!")
   # case we type python main.py help
   elif sys.argv[1].lower() == 'help':
-    print('\nWelcome to vibora :) A PDF tool that lets you convert a PDF to PNG, PDF to text, plus some more awesome things!\n\nPDF TO PNG:\nto convert a .PDF to .PNG, use: python main.py pdf2png file.pdf')
+    print('\nWelcome to vibora :) A PDF tool that lets you convert a PDF to PNG, PDF to text, plus some more awesome things. See below!\n\nPDF TO PNG:\nto convert a .PDF to .PNG, use: python main.py pdf2png file.pdf')
+    print('Remember to provide the full path to the file, and do not forget to add the .pdf at the end ;)')
     exit()
   # case we actually pass a valid argument
   else:
+    # a command is the arg after the filename. e.g. python main.py pdf2png(command) [filename]
     command = sys.argv[1].lower()
+    
+    # convert pdf to png
     # e.g. python main.py pdf2png | note that it also expects a file! -> python main.py pdf2png file.pdf
     if command == 'pdf2png':
       pdf_path = sys.argv[2]
-      print("converting")
+      # check if file exists
+      file_exists = os.path.isfile(pdf_path)
+      if not file_exists:
+        print('File not found. Is the path correct?')
+        exit()
+      print("Converting your file. Just a second...")
       pdf_to_png(pdf_path)
       time.sleep(2)
-      print('file converted!')
+      print('File converted!')
+    
+    
