398643bb61be115c91ad7aab84cfa6fd9715c5b3
Renan Araújo <renan.araujoonline@gmail.com>
2023-04-12 10:08:31 -0300

    added: merge all files in directory | read commit+

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 
 <h1 align=center><b>vibora</b>, a PDF manipulation tool.</h1>
 
-## With just one line you can convert a .PDF file to .PNG, .PNG to .PDF, merge files, encrypt and much more.
+### With just one line you can convert a .PDF file to .PNG, .PNG to .PDF, merge files, encrypt and much more.
 <hr>
 
 #### *vibora is working, but it's still an unstable tool. That means that you can use but, you might find some bugs. And if you do, feel free to open an Issue here.*
@@ -25,5 +25,5 @@ Now just run the ```vibora.py``` file to see what you can do with vibora:
 python vibora.py help
 ```
 
-
+*Python 3.10+ required.*
 *If you are using Windows, you can go to the releases and follow the instructions there. But I highly recommend you to follow this tutorial instead.*
--- vibora.py
+++ vibora.py
@@ -73,14 +73,24 @@ def compress_pdf(pdf_path):
     writer.write(f)
 
 # merge pdf files
-def merge_pdf(file_one, file_two):
+def merge_pdf(*pdf_files):
   merger = PdfMerger()
-  pdf_files = [file_one, file_two]
   for pdf_file in pdf_files:
-    merger.append(pdf_file)
+    with open(pdf_file, 'rb') as f:
+      merger.append(f)
   merger.write("merged_pdf.pdf")
   merger.close()
 
+# merge pdf files inside folder, ignores non .pdf files
+def merge_pdf_directory(directory_path):
+  merger = PdfMerger()
+  pdf_files = [file for file in os.listdir(directory_path) if file.endswith('.pdf')]
+  for pdf_file in pdf_files:
+    with open(os.path.join(directory_path, pdf_file), 'rb') as f:
+      merger.append(f)
+  merger.write(os.path.join(directory_path, 'merged_file.pdf'))
+  merger.close()
+    
 # rename files
 def rename_file(file, new_name):
   os.rename(file, new_name)
@@ -171,205 +181,208 @@ if __name__ == '__main__':
   # case we type vibora help
   elif sys.argv[1].lower() == 'help':
     print('\nWelcome to vibora :) A PDF tool that lets you convert a PDF to PNG, PDF to text, plus some more awesome things. See below!')
-    print("\nPDF TO PNG:\n   To convert a .PDF to .PNG, use: 'vibora pdf2png [file].pdf'")
+    print("\nPDF TO PNG:\n   To convert a .PDF to .PNG, use: 'vibora -pdf2png [file].pdf'")
     print('   Remember to provide the full path to the file, and do not forget to add the .pdf at the end ;)')
-    print("\nPDF TO TEXT:\n   To convert a .PDF to .TXT, use: 'vibora pdf2text [file].pdf'")
+    print("\nPDF TO TEXT:\n   To convert a .PDF to .TXT, use: 'vibora -pdf2text [file].pdf'")
     print('   Remember to provide the full path to the file, and do not forget to add the .pdf at the end ;)')
-    print("\nEXTRACT IMAGES FROM PDF:\n   To extract images from a .PDF file, use: 'vibora extractimg [file].pdf'")
+    print("\nEXTRACT IMAGES FROM PDF:\n   To extract images from a .PDF file, use: 'vibora -extractimg [file].pdf'")
     print('   You will be prompted with the amount of images found, and if you want to proceed or not.')
-    print("\nCOMPRESS PDF:\n   To compress a .PDF file, use: 'vibora compress [file].pdf'")
+    print("\nCOMPRESS PDF:\n   To compress a .PDF file, use: 'vibora -compress [file].pdf'")
     print('   It will try to compress your file without losing quality or removing content.')
-    print("\nTXT TO PDF:\n   To convert a .TXT file to .PDF, use: 'vibora txt2pdf [file].txt'")
+    print("\nTXT TO PDF:\n   To convert a .TXT file to .PDF, use: 'vibora -txt2pdf [file].txt'")
     print('   It will convert a .txt file into .pdf.')
-    print("\nMERGE PDFs:\n   To merge two .PDF files into one .PDF, use: 'vibora merge [file1].pdf [file2].pdf'")
-    print('   It will merge the two files provided, without losing quality or cutting content.')
-    print("\nRENAME FILES:\n   To rename files using vibora, you can use: 'vibora rename [file].pdf [newname].pdf'")
+    print("\nMERGE PDFs:\n   To merge .PDF files into one .PDF, use: 'vibora -merge [file1].pdf [file2].pdf [file3].pdf'")
+    print('   It will merge the files provided, without losing quality or cutting content. It can take as many files as you want.')
+    print("\nMERGE ALL PDF FILES INSIDE DIRECTORY:\n   To merge all .PDF files inside a directory, use: 'vibora -mf [directory]'")
+    print('   It will ignore non .PDF files and merge all .PDF files inside that folder. Files are merged by alphabetical order.')
+    print("\nRENAME FILES:\n   To rename files using vibora, you can use: 'vibora -rename [file].pdf [newname].pdf'")
     print('   It will change the name of the file you provided, with the name you typed after it, without affecting th file.')
-    print("\nROTATE PDF:\n   To rotate a .PDF file, you can you can use: 'vibora rotate [file].pdf'")
+    print("\nROTATE PDF:\n   To rotate a .PDF file, you can you can use: 'vibora -rotate [file].pdf'")
     print('   It will rotate you file by 90º. Depending on your file, you may want to rotate it multiple times.')
-    print("\nIMAGE TO PDF:\n   To convert an image to a .PDF file, you can you can use: 'vibora img2pdf [file].[extension]'")
+    print("\nIMAGE TO PDF:\n   To convert an image to a .PDF file, you can you can use: 'vibora -img2pdf [file].[extension]'")
     print('   It can convert multiple image formats into a .PDF file.')
-    print("\nSPLIT PDF:\n   To split a .PDF file into separated pages, you can you can use: 'vibora split [file].pdf'")
+    print("\nSPLIT PDF:\n   To split a .PDF file into separated pages, you can you can use: 'vibora -split [file].pdf'")
     print('   It will split the .PDF file into separated pages. Each page from the .PDF will be a single .PDF file.')
-    print("\nWATERMARK PDF:\n   To add watermark to a a .PDF file, you can you can use: 'vibora watermark [file].pdf [watermarkfile].pdf'")
+    print("\nWATERMARK PDF:\n   To add watermark to a a .PDF file, you can you can use: 'vibora -watermark [file].pdf [watermarkfile].pdf'")
     print('   It will add a watermark to the bottom left of the .PDF file. Remember that the watermark must also be a .PDF file.')
-    print("\nENCRYPT PDF:\n   To encrypt a .PDF file, you can you can use: 'vibora encrypt [file].pdf [password]'")
+    print("\nENCRYPT PDF:\n   To encrypt a .PDF file, you can you can use: 'vibora -encrypt [file].pdf [password]'")
     print('   It will encrypt a .pdf file by adding a password to be able to read its content.')
-    print("\nDECRYPT PDF:\n   To decrypt a .PDF file, you can you can use: 'vibora decrypt [file].pdf [password]'")
+    print("\nDECRYPT PDF:\n   To decrypt a .PDF file, you can you can use: 'vibora -decrypt [file].pdf [password]'")
     print("   It will remove the password of a pdf file. Note that it doesn't crack the .pdf file, it works only if you have the password.")
-    exit()
+    # exit()
   # case we actually pass a valid argument
   else:
     # a command is the arg after the filename. e.g. vibora pdf2png(command) [filename]
     command = sys.argv[1].lower()
     
-    # convert pdf to png
-    # e.g. vibora pdf2png file.pdf
-    if command == 'pdf2png':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print('File not found. Is the path correct?')
+    match command:
+
+      # convert pdf to png
+      # e.g. vibora pdf2png file.pdf
+      case "-pdf2png":
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print('File not found. Is the path correct?')
+          exit()
+        print("Converting your file. Just a second...")
+        pdf_to_png(pdf_path)
+        time.sleep(2)
+        print('File converted!')
         exit()
-      print("Converting your file. Just a second...")
-      pdf_to_png(pdf_path)
-      time.sleep(2)
-      print('File converted!')
-      exit()
     
-    # convert pdf to text
-    # e.g. vibora pdf2text file.pdf
-    elif command == 'pdf2txt':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print('File not found. Is the path correct?')
+      # convert pdf to text
+      # e.g. vibora pdf2text file.pdf
+      case '-pdf2txt':
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print('File not found. Is the path correct?')
+          exit()
+        print('Converting you file. Just a second...')
+        pdf_to_text(pdf_path)
+        time.sleep(2)
+        print('File converted!')
         exit()
-      print('Converting you file. Just a second...')
-      pdf_to_text(pdf_path)
-      time.sleep(2)
-      print('File converted!')
-      exit()
 
-    # extract imgs from pdf
-    # e.g. vibora extractimg file.pdf
-    elif command == 'extractimg':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print('File not found. Is the path correct?')
+      # extract imgs from pdf
+      # e.g. vibora extractimg file.pdf
+      case '-extractimg':
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print('File not found. Is the path correct?')
+          exit()
+        extract_img_from_pdf(pdf_path)
+        time.sleep(2)
+        print("Just a second...")
+        print("All done! Images extracted")
         exit()
-      extract_img_from_pdf(pdf_path)
-      time.sleep(2)
-      print("Just a second...")
-      print("All done! Images extracted")
-      exit()
 
-    # compress pdf
-    # e.g. vibora compress file.pdf
-    elif command == 'compress':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print('File not found. Is the path correct?')
-        exit()
-      compress_pdf(pdf_path)
-      print('Compressing your file, just a second...')
-      time.sleep(2)
-      print('All done! File compressed')
+      # compress pdf
+      # e.g. vibora compress file.pdf
+      case '-compress':
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print('File not found. Is the path correct?')
+          exit()
+        compress_pdf(pdf_path)
+        print('Compressing your file, just a second...')
+        time.sleep(2)
+        print('All done! File compressed')
 
-    # convert txt to pdf
-    elif command == 'txt2pdf':
-      txt_path = sys.argv[2]
-      # check if the file exists
-      file_exists = os.path.isfile(txt_path)
-      if not file_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      txt_to_pdf(txt_path)      
+      # convert txt to pdf
+      case '-txt2pdf':
+        txt_path = sys.argv[2]
+        # check if the file exists
+        file_exists = os.path.isfile(txt_path)
+        if not file_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        txt_to_pdf(txt_path)      
 
-    # merge pdfs into one pdf
-    elif command == 'merge':
-      file_one = sys.argv[2]
-      file_two = sys.argv[3]
-      # check if file exists
-      file_one_exists = os.path.isfile(file_one)
-      file_two_exists = os.path.isfile(file_two)
-      if not file_one_exists or not file_two_exists:
-        print("Couldn't find the files. Did you type correctly?")
-        exit()
-      print("Merging your files... Just a second.")
-      time.sleep(3)
-      merge_pdf(file_one, file_two)
+      # merge pdfs into one pdf
+      case '-merge':
+        pdf_files = sys.argv[2:]
+        # check if file exists
+        print("Merging your files... Just a second.")
+        time.sleep(3)
+        merge_pdf(*pdf_files)
 
-    # rename files
-    elif command == 'rename':
-      file = sys.argv[2]
-      new_name = sys.argv[3]
-      # check if file exists
-      file_exists = os.path.isfile(file)
-      if not file_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      rename_file(file, new_name)
+      # merge folder of pdfs
+      case '-mf':
+        directory = sys.argv[2]
+        merge_pdf_directory(directory)
 
-    # rotate pdf
-    elif command == 'rotate':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      print("Rotating your file... Just a second.")
-      time.sleep(3)
-      rotate_pdf(pdf_path)
-      print("File rotated!")
+      # rename files
+      case '-rename':
+        file = sys.argv[2]
+        new_name = sys.argv[3]
+        # check if file exists
+        file_exists = os.path.isfile(file)
+        if not file_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        rename_file(file, new_name)
+
+      # rotate pdf
+      case '-rotate':
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        print("Rotating your file... Just a second.")
+        time.sleep(3)
+        rotate_pdf(pdf_path)
+        print("File rotated!")
     
-    # image to pdf
-    elif command == 'img2pdf':
-      img_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(img_path)
-      if not file_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      print("Converting your file... Just a second.")
-      time.sleep(3)
-      image_to_pdf(img_path)
-      print("File converted")
+      # image to pdf
+      case '-img2pdf':
+        img_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(img_path)
+        if not file_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        print("Converting your file... Just a second.")
+        time.sleep(3)
+        image_to_pdf(img_path)
+        print("File converted")
   
-    # split pdf
-    elif command == 'split':
-      pdf_path = sys.argv[2]
-      # check if file exists
-      file_exists = os.path.isfile(pdf_path)
-      if not file_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      split_pdf(pdf_path)
-      print("Spliting your pdf files... Just a second.")
-      time.sleep(3)
-      print("PDF split into separated pages")
+      # split pdf
+      case '-split':
+        pdf_path = sys.argv[2]
+        # check if file exists
+        file_exists = os.path.isfile(pdf_path)
+        if not file_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        split_pdf(pdf_path)
+        print("Spliting your pdf files... Just a second.")
+        time.sleep(3)
+        print("PDF split into separated pages")
 
-    # add watermark to pdf
-    elif command == 'watermark':
-      pdf_path = sys.argv[2]
-      watermark = sys.argv[3]
-      # check if files exists
-      pdf_exists = os.path.isfile(pdf_path)
-      watermark_exists = os.path.isfile(watermark)
-      if not pdf_exists:
-        print("PDF file not found. Is the path correct?")
-      if not watermark_exists:
-        print("Watermark file not found. Is the path correct?")
-      watermark_pdf(pdf_path, watermark)
+      # add watermark to pdf
+      case '-watermark':
+        pdf_path = sys.argv[2]
+        watermark = sys.argv[3]
+        # check if files exists
+        pdf_exists = os.path.isfile(pdf_path)
+        watermark_exists = os.path.isfile(watermark)
+        if not pdf_exists:
+          print("PDF file not found. Is the path correct?")
+        if not watermark_exists:
+          print("Watermark file not found. Is the path correct?")
+        watermark_pdf(pdf_path, watermark)
     
-    # encrypt a pdf file
-    elif command == 'encrypt':
-      pdf_path = sys.argv[2]
-      password = sys.argv[3]
-      # check if file exists
-      pdf_exists = os.path.isfile(pdf_path)
-      if not pdf_exists:
-        print("File not found. Is the path correct?")
-        exit()
-      encrypt_pdf(pdf_path, password)
+      # encrypt a pdf file
+      case '-encrypt':
+        pdf_path = sys.argv[2]
+        password = sys.argv[3]
+        # check if file exists
+        pdf_exists = os.path.isfile(pdf_path)
+        if not pdf_exists:
+          print("File not found. Is the path correct?")
+          exit()
+        encrypt_pdf(pdf_path, password)
 
-    # decrypt a pdf file
-    elif command == 'decrypt':
-      pdf_path = sys.argv[2]
-      password = sys.argv[3]
-      # check fi file exists
-      pdf_exists = os.path.isfile(pdf_path)
-      if not pdf_exists:
-        print("File not found. Is the paht correct?")
-      decrypt_pdf(pdf_path, password)
+      # decrypt a pdf file
+      case '-decrypt':
+        pdf_path = sys.argv[2]
+        password = sys.argv[3]
+        # check fi file exists
+        pdf_exists = os.path.isfile(pdf_path)
+        if not pdf_exists:
+          print("File not found. Is the paht correct?")
+        decrypt_pdf(pdf_path, password)
 
-    # case command doesn't exist
-    else:
-      print("Command not recognized. Use 'vibora help' to see all the available commands")
+      # case command doesn't exist
+      case _:
+        print("Command not recognized. Use 'vibora help' to see all the available commands")
